b827b5d2b161efe42122738ef8304607ffe1b69e
amberpan <pankaj.ambere@gmail.com>
2020-06-21 15:04:19 -0400

    SSL authentication added to client and server

--- src/main/java/com/grpc/sample/client/GRPCClient.java
+++ src/main/java/com/grpc/sample/client/GRPCClient.java
@@ -4,16 +4,24 @@ import com.google.common.base.Stopwatch;
 import com.grpc.sample.HelloRequest;
 import com.grpc.sample.HelloResponse;
 import com.grpc.sample.HelloServiceGrpc;
+import com.grpc.sample.server.GRPCServer;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.netty.GrpcSslContexts;
+import io.grpc.netty.NettyChannelBuilder;
 
+import javax.net.ssl.SSLException;
+import java.io.File;
 import java.util.concurrent.TimeUnit;
 
 public class GRPCClient {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SSLException {
         Stopwatch watch = Stopwatch.createStarted();
-        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 9001)
-                .usePlaintext(true)
+        File cert = new File(GRPCServer.class.getClassLoader().getResource("certs/cert.pem").getFile());
+
+        ManagedChannel channel = NettyChannelBuilder.forAddress("localhost", 9002)
+                .sslContext(GrpcSslContexts.forClient()
+                .trustManager(cert).build())
                 .build();
         HelloServiceGrpc.HelloServiceBlockingStub stub
                 = HelloServiceGrpc.newBlockingStub(channel);
@@ -23,8 +31,8 @@ public class GRPCClient {
         for (int i = 0; i < 10000 ; i++) {
             Stopwatch stop = Stopwatch.createStarted();
             HelloResponse helloResponse = stub.hello(HelloRequest.newBuilder()
-                    .setFirstName("Baeldung")
-                    .setLastName("gRPC")
+                    .setFirstName("Hello")
+                    .setLastName("World")
                     .build());
             System.out.println("Time taken="+stop.stop().elapsed(TimeUnit.MILLISECONDS));
         }
--- src/main/java/com/grpc/sample/server/GRPCServer.java
+++ src/main/java/com/grpc/sample/server/GRPCServer.java
@@ -3,17 +3,35 @@ package com.grpc.sample.server;
 import com.grpc.sample.server.api.HelloServiceImpl;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
+import io.grpc.internal.IoUtils;
+import org.apache.velocity.texen.util.FileUtil;
 
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 
 public class GRPCServer {
 
     public static void main(String[] args) throws IOException, InterruptedException {
-        Server server = ServerBuilder.forPort(9001)
+
+        File cert = new File(GRPCServer.class.getClassLoader().getResource("certs/cert.pem").getFile());
+        File key = new File(GRPCServer.class.getClassLoader().getResource("certs/key.pem").getFile());
+
+        System.out.println("Certificate: "+cert.getAbsolutePath());
+        System.out.println("Key: "+key.getAbsolutePath());
+        Server server = ServerBuilder.forPort(9002)
+                .useTransportSecurity(cert,key)
                 .addService(new HelloServiceImpl()).build();
         System.out.println("Starting server..."+server);
         server.start();
         System.out.println("Server started...");
         server.awaitTermination();
+
+
+
+
+
+
+
     }
 }
